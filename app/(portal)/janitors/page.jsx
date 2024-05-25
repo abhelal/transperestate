c82f98de9632485d56ff2ddc5328e494caebc77c
Al Helal Abdullah <alhelal@Als-MacBook-Air.local>
2024-05-25 19:33:44 +0600

    added janitor

--- app/(portal)/janitors/page.jsx
+++ app/(portal)/janitors/page.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import JanitorList from "./JanitorList";
+import UserList from "@/components/UserList";
 import Search from "@/components/ui/Search";
-import CreateJanitor from "./CreateJanitorModal";
+import CreateUser from "@/components/CreateUser";
+import serverApi from "@/libs/serverApi";
 
-export default function Janitors({ searchParams }) {
+export default async function PropertyJanitor({ searchParams }) {
+  const query = searchParams?.query || "";
+  const page = Number(searchParams?.page) || 1;
+  const res = await serverApi.get("/user/janitors", { params: { query, page } }).catch((e) => {});
+  const users = res?.data?.users || [];
+  const totalPages = res?.data?.totalPages || 1;
+
   return (
     <div className="flex flex-col w-full h-full">
       <div className="pb-4">
-        <p className="text-xl font-semibold">Janitors</p>
+        <p className="text-xl font-semibold">Property Janitors</p>
         <div className="mt-2 flex justify-between">
           <Search placeholder="Search Janitor" />
-          <CreateJanitor searchParams={searchParams} />
+          <CreateUser />
         </div>
       </div>
-      <JanitorList searchParams={searchParams} />
+      <UserList users={users} totalPages={totalPages} />
     </div>
   );
 }
--- app/(portal)/tenants/page.jsx
+++ app/(portal)/tenants/page.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import UserList from "@/components/UserList";
 import Search from "@/components/ui/Search";
-import CreateTenant from "./CreateTenantModal";
-import TenantList from "./TenantList";
+import CreateUser from "@/components/CreateUser";
+import serverApi from "@/libs/serverApi";
 
-export default function Tenants({ searchParams }) {
+export default async function PropertyJanitor({ searchParams }) {
+  const query = searchParams?.query || "";
+  const page = Number(searchParams?.page) || 1;
+  const res = await serverApi.get("/user/tenants", { params: { query, page } }).catch((e) => {});
+  const users = res?.data?.users || [];
+  const totalPages = res?.data?.totalPages || 1;
+
   return (
     <div className="flex flex-col w-full h-full">
       <div className="pb-4">
-        <p className="text-xl font-semibold">Tenants</p>
+        <p className="text-xl font-semibold">Property Tenants</p>
         <div className="mt-2 flex justify-between">
-          <Search placeholder="Search Tenant" />
-          <CreateTenant searchParams={searchParams} />
+          <Search placeholder="Search Tenants" />
+          <CreateUser />
         </div>
       </div>
-      <TenantList searchParams={searchParams} />
+      <UserList users={users} totalPages={totalPages} />
     </div>
   );
 }
